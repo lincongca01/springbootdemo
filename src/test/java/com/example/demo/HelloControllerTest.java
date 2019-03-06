9301a9dc5e0839ec070c6874438661b5afbcc396
Cong Lin <lincongca@yahoo.com>
2019-03-06 14:51:21 -0500

    Add a multi-thread test in HelloControllerTest.

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -3,13 +3,22 @@ package com.example.demo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class HelloControllerTest extends AbstractTest {
+  private final String uri = "/api/hello";
+  
   @Override
   @Before
   public void setUp() {
@@ -18,25 +27,47 @@ public class HelloControllerTest extends AbstractTest {
 
   @Test
   public void helloworld() throws Exception {
-    String uri = "/api/hello";
     MockHttpServletResponse res = getResponse(uri);
-
-    int status = res.getStatus();
-    assertEquals(200, status);
     String content = res.getContentAsString();
-    assertTrue(content.length() < 40);
+    int len = content.length();
+
+    assertEquals(200, res.getStatus());
     assertTrue(content.endsWith("1"));
     
     content =getResponse(uri).getContentAsString();
-    assertTrue(content.length() < 40);
+    assertEquals(len, content.length());
     assertTrue(content.endsWith("2"));
+  }
+
+  @Test
+  public void helloworldMultiThreaded() throws Exception {
+    String content = getResponse(uri).getContentAsString();
+    assertEquals("1", content.substring(content.length()-1));
+
+    int poolsize = 2;
+    ExecutorService WORKER_THREAD_POOL = Executors.newFixedThreadPool(poolsize);
+    CountDownLatch latch = new CountDownLatch(poolsize);
+    for (int i = 0; i < poolsize; i++) {
+        WORKER_THREAD_POOL.submit(() -> {
+            IntStream.rangeClosed(1, 50).forEach(n -> {
+                try {
+                  getResponse(uri).getContentAsString();
+                } catch (Exception e) {
+                  Thread.currentThread().interrupt();
+                }
+            });
+            latch.countDown();
+        });
+    }
+    
+    // wait for the latch to be decremented by the two remaining threads
+    latch.await();
+
+    content = getResponse(uri).getContentAsString();
+    assertEquals("102", content.substring(content.length()-3));
   }
 
   private MockHttpServletResponse getResponse(String uri) throws Exception {
     return mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.TEXT_PLAIN_VALUE)).andReturn().getResponse();
-  }
-
-  public static void main(String[] args) {
-    System.getenv().keySet().stream().sorted().forEach(e -> System.out.println(e));;
   }
 }
